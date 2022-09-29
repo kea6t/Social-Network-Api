bd0336cdb6d8cb8120846c681d6cea676db856fb
kea6t <keasay26@gmail.com>
2022-09-29 15:02:02 -0400

    initialize the app

---
+++ models/Users.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+    }
+);
+
+// get total count of friendCount
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const Users = model('Pizza', UserSchema);
+
+module.exports = Users;
+
--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
-const commentRoutes = require('./comment-routes');
-
-router.use('/comments', commentRoutes);
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
-router.use('/pizzas', pizzaRoutes);
-
-module.exports = router;
